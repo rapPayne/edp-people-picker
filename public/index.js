38f9ea0d71893d453e5ae5aaa71ab2dc1b24af98
Jacob Boyce <jboyce313@gmail.com>
2024-01-19 15:16:39 -0500

    added class to all images and css to determine image size

--- public/index.js
+++ public/index.js
@@ -3,31 +3,32 @@ let unpickedPeople = [];
 let pickedPeople = [];
 let pickedPerson;
 
-window.addEventListener('DOMContentLoaded', getPeople);
+window.addEventListener("DOMContentLoaded", getPeople);
 async function getPeople() {
   const url = `/api/people`;
   pickedPerson = undefined;
   pickedPeople = [];
-  unpickedPeople = await fetch(url)
-    .then((res) => res.json());
+  unpickedPeople = await fetch(url).then((res) => res.json());
 
   drawPeople();
 }
 
 function drawPeople() {
-  const unpickedHtml = unpickedPeople.map(person => drawPerson(person)).join("");
-  document.querySelector('section#unpickedPeople').innerHTML = unpickedHtml;
-  const pickedHtml = pickedPeople.map(person => drawPerson(person)).join("");
-  document.querySelector('section#pickedPeople').innerHTML = pickedHtml;
-  document.querySelector('section#pickedPerson').innerHTML = drawPerson(pickedPerson);
-
+  const unpickedHtml = unpickedPeople
+    .map((person) => drawPerson(person))
+    .join("");
+  document.querySelector("section#unpickedPeople").innerHTML = unpickedHtml;
+  const pickedHtml = pickedPeople.map((person) => drawPerson(person)).join("");
+  document.querySelector("section#pickedPeople").innerHTML = pickedHtml;
+  document.querySelector("section#pickedPerson").innerHTML =
+    drawPerson(pickedPerson);
 }
 
 function pickRandomPerson() {
   const randomNumber = Math.floor(Math.random() * unpickedPeople.length);
   pickedPerson = unpickedPeople[randomNumber];
   pickedPeople = [...pickedPeople, pickedPerson];
-  unpickedPeople = unpickedPeople.filter(person => person !== pickedPerson);
+  unpickedPeople = unpickedPeople.filter((person) => person !== pickedPerson);
   console.log(`Person is ${pickedPerson.name}`);
   drawPeople();
 }
@@ -37,7 +38,7 @@ function drawPerson(person) {
   return `
   <div class="person" data-id="${person.id}">
    <p>${person.name}</p>
-   <img src="${person.picture}" alt="${person.name}" />
+   <img class="person-image" src="${person.picture}" alt="${person.name}" />
   </div>
   `;
 }
