110932d4753d69ac103bf8b541c5a742d3a63f0c
rapPayne <bob@creator.net>
2024-01-27 11:33:27 -0600

    Minor cleanups/refactoring

--- main.js
+++ main.js
@@ -7,14 +7,17 @@ const app = express();
 let db = JSON.parse(fs.readFileSync('db.json'));
 const people = db["people"];
 
+app.all("*", (req, res, next) => {
+  console.log(req.url);
+  next();
+});
+
 //Serve all the people at GET /people
 app.get("/api/people", (re, res) => {
   res.send(people);
 });
 
-
 app.use(express.static("public"));
 
-//TODO: Add a callback as the 2nd argument.
 const port = 3500;
 app.listen(port, () => console.log(`Listening on port ${port}.`));
