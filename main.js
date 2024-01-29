fa9291dec603abfd69a939ed60c4ad4d6f5f9c6a
Ola Ekdahl <ola@ciracon.com>
2024-01-29 13:50:34 -0800

    Update API to use MongoDB

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // The web server
 import express from 'express';
-import fs from 'fs';
+import { MongoClient } from 'mongodb';
+// import fs from 'fs';
 
 const app = express();
 
-let db = JSON.parse(fs.readFileSync('db.json'));
-const people = db["people"];
+// let db = JSON.parse(fs.readFileSync('db.json'));
+// const people = db["people"];
 
 app.all("*", (req, res, next) => {
   console.log(req.url);
@@ -13,8 +14,36 @@ app.all("*", (req, res, next) => {
 });
 
 //Serve all the people at GET /people
-app.get("/api/people", (req, res) => {
-  res.send(people);
+app.get("/api/people", async (req, res) => {
+  // res.send(people);
+  try {
+    const client = await MongoClient.connect('mongodb://localhost:27017');
+    const db = client.db('edp');
+    const collection = db.collection('people');
+    const people = await collection.find().toArray();
+    client.close();
+    res.json(people);
+  } catch (error) {
+    res.status(500).json({error: error});
+  }
+});
+
+app.get("/api/people/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const client = await MongoClient.connect('mongodb://localhost:27017');
+    const db = client.db('edp');
+    const collection = db.collection('people');
+    const person = await collection.findOne({'id': +id});
+    client.close();
+    if(person) {
+      res.json(person);
+    } else {
+      res.status(404).json({message: 'Person not found'});
+    }
+  } catch (error) {
+    res.status(500).json({error: error});
+  }
 });
 
 
